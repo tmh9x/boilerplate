a2186ecb4719742f4935278ee55687a2bd73de72
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-07 10:54:01 +0200

    :ambulance: Update Express server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
-import fs from 'fs';
 
-console.log(fs.readFileSync('./README.md', 'utf-8'));
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
@@ -10,3 +9,7 @@ app.use('/storybook', express.static('dist/storybook'));
 
 // Serve app production bundle
 app.use(express.static('dist/app'));
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
