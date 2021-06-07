733b202bedfc88e440cd04d8e04b66b63d32503a
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-07 10:17:31 +0200

    :construction_worker: Serve app production bundle

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,9 @@
+import express from 'express';
 import fs from 'fs';
 
 console.log(fs.readFileSync('./README.md', 'utf-8'));
+
+const app = express();
+
+// Serve app production bundle
+app.use(express.static('dist/app'));
