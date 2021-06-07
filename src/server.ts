f31c9e30ff42f117c3ca675dbd00865e45de3b6f
tmh9x <79111136+tmh9x@users.noreply.github.com>
2021-06-07 10:30:51 +0200

    :construction_worker: Serve storybook

--- src/server.ts
+++ src/server.ts
@@ -5,5 +5,8 @@ console.log(fs.readFileSync('./README.md', 'utf-8'));
 
 const app = express();
 
+// Serve storybook production bundle
+app.use('/storybook', express.static('dist/storybook'));
+
 // Serve app production bundle
 app.use(express.static('dist/app'));
